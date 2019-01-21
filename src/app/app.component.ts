52b77120e2788b6d1ddde8f19ffa0fd73407b974
Andreu García Cuevas <agarcue9@gmail.com>
2019-01-21 12:35:13 +0100

    css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,11 @@ export class AppComponent {
   title = 'proyectoFinalAngular';
   comprobar = false;
   // arrayOpacidades = [0,0,0,0,0,0,0,0];
-  arrayImagenes = ['asce.jpg', 'kaydo.jpg', 'shanks.jpg', 'trafalgar.jpg', 'asce.jpg', 'kaydo.jpg', 'shanks.jpg', 'trafalgar.jpg'];
+  myArray = ['asce.jpg', 'kaydo.jpg', 'shanks.jpg', 'trafalgar.jpg', 'asce.jpg', 'kaydo.jpg', 'shanks.jpg', 'trafalgar.jpg']; 
+  
+  constructor (){
+    this.shuffle();
+  }
 
   // posicionFilaInicial = [[false,false,false]];
 
@@ -26,42 +30,34 @@ export class AppComponent {
   //   }
 
   
-  reiniciarJuego() {
-    var juego = document.getElementById("juego");
-    juego.style.opacity = "0.3";
+  barajarCartas() {
+    let random = this.myArray[Math.floor(Math.random() * this.myArray.length)];
+    console.log(random)
   }
 
-  levantarCartas(event) {
+  sacarId(event) {
     let identificar = event.target.id;
     console.log(identificar);
-    // let identificar = document.getElementById();
-    // identificar.style.opacity="0";
+    // let random = this.myArray[Math.floor(Math.random() * this.myArray.length)];
+    // console.log(random)
+  
+  }
+
+  repartirImagenes(){
+    var posicion = document.getElementById("piece1")
+      posicion.style.opacity="1";
+    
+  }
+  
+  shuffle() {
+    for (let index = this.myArray.length - 1; index >= 0; index--) {
+      let numRandom = Math.floor(Math.random() * index)
+      let auxiliar = this.myArray[numRandom];
+      this.myArray[numRandom] = this.myArray[index];
+      this.myArray[index] = auxiliar;
+    }
   }
 }
-
-// createReferenceSolution() {
-//   let posicion = [];
-//   // si i es menor que el numero de piezas.... 
-//   for (let i = 0; i < numPiezas; i++) {
-//     posicion.push(pieceToOffset(i,anchura,altura,numPiezas));
-//   }
-//   return posicion;
-// }
-
-
-  
-
-//   shuffle(arrayImagenes) {
-//     // arrayImagenes = ['asce.jpg','kaydo.jpg','shanks.jpg','trafalgar.jpg','asce.jpg','kaydo.jpg','shanks.jpg','trafalgar.jpg'];
-//     // inicializamos el array en el length -1 y lo recorremos del final al principio
-//     for (let index = arrayImagenes.length - 1; index >= 0; index--) {
-//       let numRandom = Math.floor(Math.random() * index)
-//       let auxiliar = arrayImagenes[numRandom];
-//       arrayImagenes[numRandom] = arrayImagenes[index];
-//       arrayImagenes[index] = auxiliar;
-//     }
-//   }
-
 //   drawContentPuzzle(posicion) {
 //     for (let i = 0; i < posicion.length; i++) {
 //       document.getElementById('piece'+i).style.backgroundPosition = posicion[i][0]+'px '+posicion[i][1]+'px';
